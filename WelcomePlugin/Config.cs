4801b35ee80d9a7a5a455c0cba97a75548b2fa48
ZDC <88444937+Aquarty@users.noreply.github.com>
2023-09-10 21:17:28 +0700

    Update Config & Version

--- WelcomePlugin/Config.cs
+++ WelcomePlugin/Config.cs
@@ -1,14 +1,25 @@
 using Nebuli.API.Interfaces;
-using System.ComponentModel;
+using System;
+using System.Collections.Generic;
 
 namespace WelcomePlugin
 {
+    public class BroadcastConfig
+    {
+        public string Content { get; set; } = "<color=#6EDAFF><size=55>Welcome <b>%player%!</b></size> \n<i>Make sure to read our rules! \nJoin our discord if you enjoy the server!</i></color>";
+        public ushort Duration { get; set; } = 6;
+        public bool Show { get; set; } = true;
+    }
+
     public class Config : IConfiguration
     {
         public bool IsEnabled { get; set; } = true;
         public bool Debug { get; set; } = false;
-        [Description("Message Configuration")]
-        public string BroadcastMessage { get; set; } = "<color=#6EDAFF><size=55>Welcome <b>%player%!</b></size> \n<i>Make sure to read our rules! \nJoin our discord if you enjoy the server!</i></color>";
-        public ushort BroadcastDuration { get; set; } = 6;
+        public Dictionary<string, BroadcastConfig> BroadcastsManager { get; set; } = new Dictionary<string, BroadcastConfig>
+        {
+            {
+                "WelcomeBroadcast", new BroadcastConfig()
+            }
+        };
     }
 }
--- WelcomePlugin/Plugin.cs
+++ WelcomePlugin/Plugin.cs
@@ -1,6 +1,10 @@
 using System;
+using System.Collections;
+using System.Collections.Generic;
+using MEC;
 using Nebuli.API.Features;
 using Nebuli.Events.EventArguments.Player;
+using Nebuli.Events.EventArguments.Round;
 using Nebuli.Events.Handlers;
 
 namespace WelcomePlugin
@@ -9,31 +13,40 @@ namespace WelcomePlugin
     public class Plugin : Plugin<Config>
     {
         public static Plugin Instance { get; set; }
-        public override string Name => "WelcomeBroadcast";
+        public override string Name => "BroadcastManager";
         public override string Creator => "Heart";
         public override Version Version { get; } = new(1, 0, 0);
-        public override Version NebuliVersion { get; } = new(1, 2, 2);
+        public override Version NebuliVersion { get; } = new(1, 2, 3);
 
         public override void OnEnabled()
         {
             Instance = this;
-            PlayerHandlers.Join += RunBroadcast;
+            PlayerHandlers.Join += WelcomeBroadcast;
             base.OnEnabled();
         }
 
         public override void OnDisabled()
         {
-            PlayerHandlers.Join -= RunBroadcast;
+            PlayerHandlers.Join -= WelcomeBroadcast;
             base.OnDisabled();
             Instance = null;
         }
 
-        private void RunBroadcast(PlayerJoinEvent ev)
+        private void WelcomeBroadcast(PlayerJoinEvent ev)
         { 
-            Log.Debug($"Send Broadcast Message to {ev.Player.Nickname} ({ev.Player.UserId})");
-            
-            Config.BroadcastMessage = Config.BroadcastMessage.Replace("%player%", ev.Player.Nickname);
-            ev.Player.Broadcast(Config.BroadcastMessage, Config.BroadcastDuration);
+            Log.Debug($"Send Welcome Broadcast Message to {ev.Player.Nickname} ({ev.Player.UserId})");
+            if (Config.BroadcastsManager.TryGetValue("WelcomeBroadcast", out var welcomeBroadcastConfig))
+            {
+                if (welcomeBroadcastConfig.Show)
+                {
+                    var broadcastMessage = welcomeBroadcastConfig.Content.Replace("%player%", ev.Player.Nickname);
+                    ev.Player.Broadcast(broadcastMessage, welcomeBroadcastConfig.Duration);
+                }
+            }
+            else
+            {
+                Log.Error("Welcome broadcast configuration not found.");
+            }
         }
     }
 }
